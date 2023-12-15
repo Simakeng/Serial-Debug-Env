dd0a06042dfc49bf99ffece3d1c5d7bc17505328
Simakeng <simakeng@outlook.com>
2023-12-15 15:07:12 +0800

    Modify the project structure.
        modified:   Serial-Debug-Env.sln
        modified:   Serial-Debug-Env.vcxproj
        modified:   Serial-Debug-Env.vcxproj.filters
        new file:   env/env.c
        renamed:    env/example.c -> env/examples/helloworld/helloworld.c
        new file:   env/examples/helloworld/helloworld.vcxproj
        new file:   env/examples/helloworld/helloworld.vcxproj.filters

---
+++ env/env.c
@@ -0,0 +1,11 @@
+#include <port_api.h>
+#include <timer_api.h>
+#include <debug_api.h>
+
+int app_main(int argc, char *argv[])
+{
+    /***********************************************
+     *      implement your application here        *
+     **********************************************/
+    return 0;
+}
